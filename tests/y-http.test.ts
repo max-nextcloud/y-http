1f541e23e72349baa4f9c2af8b6ccf1617316bdd
Max <max@nextcloud.com>
2025-06-05 17:12:01 +0200

    test: move DummyServer into separate file
    Signed-off-by: Max <max@nextcloud.com>

--- tests/y-http.test.ts
+++ tests/y-http.test.ts
@@ -4,30 +4,7 @@ import { beforeEach, expect, test, vi } from 'vitest'
 import { fromBase64, } from 'lib0/buffer.js'
 import { Decoder, readUint8 } from 'lib0/decoding.js'
 import { HttpProvider } from '../src/y-http'
-
-class DummyServer {
-    storage = new Map<number, string[]>()
-    version = 0
-
-    constructor(data?: string[]) {
-        if (data) {
-            this.receive(data)
-        }
-    }
-
-    receive(data: string[]) {
-        this.version += Math.floor(Math.random() * 100)
-        this.storage.set(this.version, data)
-    }
-
-    respond(since: number): { data: string[], version: number } {
-        const data = Array.from(this.storage.entries())
-            .filter(([id, _data]) => id >= since)
-            .map(([_id, data]) => data)
-            .flat()
-        return { data, version: this.version }
-    }
-}
+import { DummyServer } from './DummyServer.ts'
 
 function mockApi(server?: DummyServer) {
     const _connection = {}
