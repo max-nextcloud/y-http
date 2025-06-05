11513419676c69c5cb820a960b649f41668f05b5
Max <max@nextcloud.com>
2025-06-05 16:50:38 +0200

    test(yjs): split from y-http test
    Signed-off-by: Max <max@nextcloud.com>

--- src/y-http.test.ts
+++ src/y-http.test.ts
@@ -1,8 +1,7 @@
 import * as Y from 'yjs'
 import * as sync from 'y-protocols/sync'
-import { Encoder, toUint8Array, writeUint8 } from 'lib0/encoding.js'
 import { beforeEach, expect, test, vi } from 'vitest'
-import { fromBase64, toBase64 } from 'lib0/buffer.js'
+import { fromBase64, } from 'lib0/buffer.js'
 import { Decoder, readUint8 } from 'lib0/decoding.js'
 import { HttpProvider } from './y-http'
 
@@ -32,8 +31,6 @@ class DummyServer {
     }
 
     respond(since: number): { data: string[], version: number } {
-        // Todo: figure out how to use Iterator.filter
-        // https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Iterator/filter
         const data = Array.from(this.storage.entries())
             .filter(([id, _data]) => id >= since)
             .map(([_id, data]) => data)
@@ -153,60 +150,6 @@ test('applies updates received after from send', async () => {
 })
 
 // TODO: Check we do not resend received updates
-
-test('Setting values on a XmlFragment', () => {
-    const ydoc = new Y.Doc()
-    const frag = ydoc.getXmlFragment('prosemirror')
-    expect(frag).toBeInstanceOf(Y.XmlFragment)
-    expect(frag.length).toBe(0)
-    const elem = new Y.XmlElement('paragraph')
-    frag.insert(0, [elem])
-    expect(frag.length).toBe(1)
-    expect(frag.toArray()[0]).toBe(elem)
-    expect(frag).toMatchInlineSnapshot(`"<paragraph></paragraph>"`)
-})
-
-test('sync between two docs', () => {
-    const source = new Y.Doc()
-    const dest = new Y.Doc()
-    source.on('update', update => {
-        Y.applyUpdate(dest, update)
-    })
-    update(source)
-    expect(dest).toEqual(source)
-    update(dest)
-    expect(source).not.toEqual(dest)
-})
-
-test('two way sync', () => {
-    const doc1 = new Y.Doc()
-    const doc2 = new Y.Doc()
-    doc1.on('update', update => {
-        Y.applyUpdate(doc2, update)
-    })
-    doc2.on('update', update => {
-        Y.applyUpdate(doc1, update)
-    })
-    update(doc1)
-    update(doc2)
-    expect(doc1).toEqual(doc2)
-})
-
-test('using send with y-protocols', () => {
-    const doc1 = new Y.Doc()
-    const send = vi.fn()
-    doc1.on('update', update => {
-        const enc = new Encoder()
-        writeUint8(enc, 0)
-        sync.writeUpdate(enc, update)
-        const data = toUint8Array(enc)
-        send(toBase64(data))
-    })
-    update(doc1)
-    expect(send).toHaveBeenCalled()
-    const data = send.mock.lastCall?.[0]
-    expect(data.slice(0, 3)).toMatchInlineSnapshot(`"AAI"`)
-})
 
 let _updateCount = 0
 function update(doc: Y.Doc) {
---
+++ src/yjs.test.ts
@@ -0,0 +1,80 @@
+import * as Y from 'yjs'
+import * as sync from 'y-protocols/sync'
+import { Encoder, toUint8Array, writeUint8 } from 'lib0/encoding.js'
+import { beforeEach, expect, test, vi } from 'vitest'
+import { toBase64 } from 'lib0/buffer.js'
+
+function areDocsEqual(a: unknown, b: unknown): boolean | undefined {
+    const isADoc = a instanceof Y.Doc
+    const isBDoc = b instanceof Y.Doc
+    if (isADoc && isBDoc) {
+        return JSON.stringify(a.getMap())
+            === JSON.stringify(b.getMap())
+    }
+}
+expect.addEqualityTesters([areDocsEqual])
+
+beforeEach(() =>
+    vi.resetAllMocks()
+)
+
+// TODO: Check we do not resend received updates
+
+test('Setting values on a XmlFragment', () => {
+    const ydoc = new Y.Doc()
+    const frag = ydoc.getXmlFragment('prosemirror')
+    expect(frag).toBeInstanceOf(Y.XmlFragment)
+    expect(frag.length).toBe(0)
+    const elem = new Y.XmlElement('paragraph')
+    frag.insert(0, [elem])
+    expect(frag.length).toBe(1)
+    expect(frag.toArray()[0]).toBe(elem)
+    expect(frag).toMatchInlineSnapshot(`"<paragraph></paragraph>"`)
+})
+
+test('sync between two docs', () => {
+    const source = new Y.Doc()
+    const dest = new Y.Doc()
+    source.on('update', update => {
+        Y.applyUpdate(dest, update)
+    })
+    update(source)
+    expect(dest).toEqual(source)
+    update(dest)
+    expect(source).not.toEqual(dest)
+})
+
+test('two way sync', () => {
+    const doc1 = new Y.Doc()
+    const doc2 = new Y.Doc()
+    doc1.on('update', update => {
+        Y.applyUpdate(doc2, update)
+    })
+    doc2.on('update', update => {
+        Y.applyUpdate(doc1, update)
+    })
+    update(doc1)
+    update(doc2)
+    expect(doc1).toEqual(doc2)
+})
+
+test('using send with y-protocols', () => {
+    const doc1 = new Y.Doc()
+    const send = vi.fn()
+    doc1.on('update', update => {
+        const enc = new Encoder()
+        writeUint8(enc, 0)
+        sync.writeUpdate(enc, update)
+        const data = toUint8Array(enc)
+        send(toBase64(data))
+    })
+    update(doc1)
+    expect(send).toHaveBeenCalled()
+    const data = send.mock.lastCall?.[0]
+    expect(data.slice(0, 3)).toMatchInlineSnapshot(`"AAI"`)
+})
+
+let _updateCount = 0
+function update(doc: Y.Doc) {
+    doc.getMap().set(`update-${_updateCount++}`, 'world')
+}
